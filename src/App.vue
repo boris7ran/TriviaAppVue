d11e05c46c08cbe655efcefe4396565a1c1aa3d5
boris7ran <boris.ran7@gmail.com>
2019-07-19 18:21:04 +0200

    Zadatak 1

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light"> 
+      <router-link to="/" class="navbar-brand">TriviaApp</router-link>
+    </nav>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
